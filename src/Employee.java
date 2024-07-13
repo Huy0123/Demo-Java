56ac47962e057df4feb4fd0e6275503b44aaa54d
Võ Quốc Huy <130681293+Huy0123@users.noreply.github.com>
2024-07-14 00:31:42 +0700

    Update Employee.java
    tdfgdf

--- src/Employee.java
+++ src/Employee.java
@@ -8,7 +8,7 @@ public abstract class Employee {
         this.lastName = lastName;
         this.socialSecurityNumber = SSN;
     }
-// ffwfwf
+
     public void print() {
         System.out.println(STR."\{firstName} \{lastName}");
         System.out.println(STR."social security number: \{socialSecurityNumber}");
